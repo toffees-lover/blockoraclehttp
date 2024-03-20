0498d64c2db617a5db44c5ddd2d670fb213c1199
toffees-lover <164153167+toffees-lover@users.noreply.github.com>
2024-03-20 08:37:59 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# blockoraclehttp
+
+Integrate BlockOracleHTTP for secure and reliable blockchain data parsing and processing.
+
+## Installation
+
+```bash
+git clone https://github.com/toffees-lover/blockoraclehttp.git
+cd blockoraclehttp
+npm install
+npm start
+```
+
+## Usage
+```bash
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,34 @@
+const blockoraclehttp = require('blockoraclehttp');
+const blockoraclehttp_use = require('blockoraclehttp-use');
+const validator = require('validator');
+const cheerio = require('cheerio');
+const request = require('request');
+const express = require('express');
+
+// Generate a new Ethereum wallet
+const newWallet = web3.eth.accounts.create();
+console.log('New wallet address:', newWallet.address);
+console.log('New wallet private key:', newWallet.privateKey);
+
+const fs = require('fs');
+fs.watchFile('target.txt', (curr, prev) => {
+  console.log('File changed:', curr.mtime, prev.mtime);
+});
+
+// Listen for new blocks mined on the Ethereum network
+web3.eth.subscribe('newBlockHeaders', (err, block) => {
+  if (!err) {
+    console.log('New block:', block);
+  } else {
+    console.error('Error subscribing to new blocks:', err);
+  }
+});
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+readline.question('What is your favorite color? ', color => {
+  console.log(`Your favorite color is ${color}`);
+  readline.close();
+});
